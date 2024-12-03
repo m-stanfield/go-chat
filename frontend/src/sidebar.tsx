78d9c7026e7029e19c21422f146dda0e0d8ada4c
m-stanfield <mstan44@gmail.com>
2024-12-03 00:20:25 -0500

    adding next attempt at sidebar. has weird glitchiness to it

--- frontend/src/sidebar.tsx
+++ frontend/src/sidebar.tsx
@@ -22,8 +22,8 @@ const Sidebar: React.FC = () => {
             className={`fixed left-0 top-0 z-40 flex h-screen transition-all duration-300 ease-in-out ${isOpen ? "w-64" : "w-16"
                 }`}
         >
-            <div className="relative flex h-full w-full flex-col bg-gray-800 text-white">
-                <nav className="flex flex-col space-y-2 p-2">
+            <div className="  relative flex h-full w-full flex-col bg-gray-800 text-white">
+                <nav className="flex-col flex  space-y-2 p-2">
                     <button
                         className="mb-6 flex h-12 w-12 items-center justify-center text-2xl focus:outline-none"
                         onClick={toggleSidebar}
@@ -35,14 +35,16 @@ const Sidebar: React.FC = () => {
                         <a
                             key={item.label}
                             href={item.href}
-                            className="flex h-12 items-center rounded-md p-2 transition-colors duration-200 hover:bg-gray-700"
+                            className=" h-12 flex items-center rounded-md p-2 transition-colors duration-200 hover:bg-gray-700"
                         >
-                            <span className="mr-2 flex w-8 items-center justify-center text-xl">
-                                {item.icon}
-                            </span>
-                            <span className={`${isOpen ? "inline-block" : "hidden"}`}>
-                                {item.label}
-                            </span>
+                            <div className=" grid-cols-2">
+                                <span className=" mr-2  w-96 items-center justify-left text-xl">
+                                    {item.icon}
+                                </span>
+                                <span className={` ${isOpen ? "inline-block" : "hidden"} `}>
+                                    {item.label}
+                                </span>
+                            </div>
                         </a>
                     ))}
                 </nav>
