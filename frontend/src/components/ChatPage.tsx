5fba9efbac15fdbbf93c71575a7202f7f50308c4
m-stanfield <mstan44@gmail.com>
2024-12-18 00:44:29 -0500

    moving logic out of chatpage into server page

--- frontend/src/components/ChatPage.tsx
+++ frontend/src/components/ChatPage.tsx
@@ -1,90 +1,17 @@
 import MessageSubmitWindow from "./MessageSubmitWindow";
 import { SyntheticEvent, useEffect, useRef, useState } from "react";
 import Message, { MessageData } from "./Message"; // Import your Message component
-
-import MockMessages from "./MockMessages";
 import { useAuth } from "../AuthContext";
 
 interface ChatPageProps {
     channel_id: number;
+    messages: MessageData[];
+    onSubmit: (t: SyntheticEvent, inputValue: string) => string;
 }
-function ChatPage({ channel_id }: ChatPageProps) {
+function ChatPage({ channel_id, messages, onSubmit }: ChatPageProps) {
     const auth = useAuth();
-    const [messages, setMessages] = useState<MessageData[]>(MockMessages);
-    const ws = useRef<WebSocket | null>(null);
-    const dummy = useRef<HTMLDivElement | null>(null);
-    const [focusMessageWindow, setFocuseMessageWindow] = useState(false);
-    const maxMessageLength = 30;
-
-    useEffect(() => {
-        const startTime = Date.now();
-        console.log("starting to open websocket", Date.now() - startTime);
-        ws.current = new WebSocket(`ws://localhost:8080/websocket`);
-        ws.current.onopen = () => {
-            console.log("opening ws", Date.now() - startTime);
-        };
-        ws.current.onmessage = function(event: MessageEvent) {
-            const json = JSON.parse(event.data);
-            try {
-                const newMessage: MessageData = {
-                    id: json.messageid,
-                    channel_id: channel_id,
-                    message: json.message,
-                    date: new Date(json.date),
-                    author: json.username,
-                    author_id: json.userid,
-                };
-                setMessages((messages) => {
-                    console.log(messages.length);
-                    if (messages.length > maxMessageLength) {
-                        return [...messages.slice(-maxMessageLength), newMessage];
-                    }
-                    return [...messages, newMessage];
-                });
-            } catch (err) {
-                console.log(err);
-            }
-
-            if (focusMessageWindow) {
-                console.log("updating focus message: " + focusMessageWindow);
-                setFocuseMessageWindow(false);
-                if (dummy.current) {
-                    dummy.current.scrollIntoView({ behavior: "smooth" });
-                }
-            }
-        };
-        ws.current.onclose = () => {
-            console.log("ws closed", Date.now() - startTime);
-        };
-
-        return () => {
-            ws.current?.close();
-        };
-    }, []);
-
-    const onSubmit = (t: SyntheticEvent, inputValue: string): string => {
-        t.preventDefault();
-        if (inputValue.length === 0) {
-            return inputValue;
-        }
-        setFocuseMessageWindow(true);
-        const stringified = JSON.stringify({
-            channel_id: channel_id,
-            message: inputValue,
-        });
-        if (ws?.current === null) {
-            console.log("websocket hasn't be initialized yet");
-            return inputValue;
-        }
-        if (ws.current.readyState === WebSocket.CLOSED) {
-            console.log("can't send ws closed");
-            return inputValue;
-        }
-        ws.current.send(stringified);
-        return "";
-    };
-
     const messageEndRef = useRef(null);
+
     useEffect(() => {
         const lastMessage = messages[messages.length - 1];
         if (lastMessage.author_id == auth.authState.user?.id) {
@@ -92,7 +19,7 @@ function ChatPage({ channel_id }: ChatPageProps) {
                 behavior: "smooth",
             });
         }
-    }, [messages]);
+    }, [auth.authState.user?.id, messages]);
     return (
         <div className=" flex h-full w-full flex-col ">
             <div className="w-full">Channel ID: {channel_id}</div>
--- frontend/src/components/ServerPage.tsx
+++ frontend/src/components/ServerPage.tsx
@@ -1,16 +1,86 @@
 import ChatPage from "./ChatPage";
+import { SyntheticEvent, useEffect, useRef, useState } from "react";
+import MockMessages from "./MockMessages";
+import { MessageData } from "./Message";
 
 interface ServerPageProps {
     server_id: number;
 }
 function ServerPage({ server_id }: ServerPageProps) {
+    const selectedChannelId = 2;
+    const [messages, setMessages] = useState<MessageData[]>(MockMessages);
+    const ws = useRef<WebSocket | null>(null);
+    const maxMessageLength = 30;
+    const onSubmit = (t: SyntheticEvent, inputValue: string): string => {
+        t.preventDefault();
+        if (inputValue.length === 0) {
+            return inputValue;
+        }
+        const stringified = JSON.stringify({
+            channel_id: selectedChannelId,
+            message: inputValue,
+        });
+        if (ws === null) {
+            console.log("websocket hasn't be initialized yet");
+            return inputValue;
+        }
+        if (ws.current?.readyState === WebSocket.CLOSED) {
+            console.log("can't send ws closed");
+            return inputValue;
+        }
+        ws.current?.send(stringified);
+        return "";
+    };
+
+    useEffect(() => {
+        const startTime = Date.now();
+        console.log("starting to open websocket", Date.now() - startTime);
+        ws.current = new WebSocket(`ws://localhost:8080/websocket`);
+        ws.current.onopen = () => {
+            console.log("opening ws", Date.now() - startTime);
+        };
+        ws.current.onmessage = function(event: MessageEvent) {
+            const json = JSON.parse(event.data);
+            try {
+                const newMessage: MessageData = {
+                    id: json.messageid,
+                    channel_id: json.channel_id,
+                    message: json.message,
+                    date: new Date(json.date),
+                    author: json.username,
+                    author_id: json.userid,
+                };
+                setMessages((messages) => {
+                    console.log(messages.length);
+                    if (messages.length > maxMessageLength) {
+                        return [...messages.slice(-maxMessageLength), newMessage];
+                    }
+                    return [...messages, newMessage];
+                });
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        ws.current.onclose = () => {
+            console.log("ws closed", Date.now() - startTime);
+        };
+
+        return () => {
+            ws.current?.close();
+        };
+    }, []);
+
     return (
         <div className="w-full h-full">
             <div>
                 <h1 className="w-full">Server ID: {server_id}</h1>
             </div>
             <div className="h-full w-full">
-                <ChatPage channel_id={2} />
+                <ChatPage
+                    channel_id={selectedChannelId}
+                    onSubmit={onSubmit}
+                    messages={messages}
+                />
             </div>
         </div>
     );
