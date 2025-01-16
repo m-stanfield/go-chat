803894b09e772f03dfd7836f8c59bbae304ba465
m-stanfield <mstan44@gmail.com>
2025-01-15 22:06:04 -0500

    adding server list from server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,12 +11,55 @@ function App() {
       console.log("logout callback");
     });
   }, []);
-  const servers_ids = [1, 2].map((x) => ({
-    server_id: x,
-  }));
-  const [selectedServerId, setSelectedServerId] = useState<ServerInfo>(
-    servers_ids[0],
-  );
+
+  useEffect(() => {
+    const _call = async () => {
+      if (auth.authState.user?.id === undefined) {
+        return;
+      }
+
+      try {
+        // Send POST request to backend
+        const response = await fetch(
+          `http://localhost:8080/api/user/${auth.authState.user?.id}/servers`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+          },
+        );
+        if (response.ok) {
+          const data = await response.json();
+          const servers_ids: ServerInfo[] = data.servers.map((server: any) => {
+            return {
+              server_id: server.ServerId,
+              server_name: server.ServerName,
+              owner_id: server.OwnerId,
+            };
+          });
+          setServerId(servers_ids);
+          if (servers_ids.length > 0) {
+            setSelectedServerId(servers_ids[0]);
+          }
+        } else {
+          console.error("Login failed:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error submitting login:", error);
+        return;
+      }
+    };
+    _call();
+  }, [auth.authState.user]);
+
+  const [servers_ids, setServerId] = useState<ServerInfo[]>([]);
+  const [selectedServerId, setSelectedServerId] = useState<ServerInfo>({
+    server_id: 0,
+    server_name: "",
+    owner_id: 0,
+  });
   function onServerSelect(server_id: number) {
     const selectedServer = servers_ids.find((x) => x.server_id === server_id);
     if (selectedServer !== undefined) {
