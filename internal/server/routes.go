6e6961d560a863febcf3377d9e86e23b47e3e47c
m-stanfield <mattstanfield52@gmail.com>
2025-03-12 21:58:08 -0400

    removing error out of verification server

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -118,66 +118,61 @@ type ServerVerification struct {
 	Validated bool
 	UserId    database.Id
 	Server    database.Server
-	Httperr   *HttpErrorInfo
 }
 
-func (s *Server) verifyServerOwner(r *http.Request) ServerVerification {
+func (s *Server) verifyServerOwner(r *http.Request) (ServerVerification, *HttpErrorInfo) {
 	userid, err := getUserIdFromContext(r)
 	if err != nil {
 		return ServerVerification{
-			Validated: false,
-			Httperr: &HttpErrorInfo{
+				Validated: false,
+			}, &HttpErrorInfo{
 				StatusCode: http.StatusInternalServerError,
 				Message:    "unable to get userid from context",
-			},
-		}
+			}
 	}
 	serverid_str := r.PathValue("serverid")
 	serverid, err := strconv.Atoi(serverid_str)
 	if err != nil {
 		return ServerVerification{
-			Validated: false,
-			Httperr: &HttpErrorInfo{
+				Validated: false,
+			}, &HttpErrorInfo{
 				StatusCode: http.StatusBadRequest,
 				Message:    "invalid request: unable to parse server id",
-			},
-		}
+			}
 	}
 	if serverid <= 0 {
 		return ServerVerification{
-			Validated: false,
-			Httperr: &HttpErrorInfo{
+				Validated: false,
+			},
+			&HttpErrorInfo{
 				StatusCode: http.StatusBadRequest,
 				Message:    "invalid request: invalid server id",
-			},
-		}
+			}
 	}
 	server_info, err := s.db.GetServer(database.Id(serverid))
 	if err != nil {
 		return ServerVerification{
-			Validated: false,
-			Httperr: &HttpErrorInfo{
+				Validated: false,
+			},
+			&HttpErrorInfo{
 				StatusCode: http.StatusBadRequest,
 				Message:    "error: unable to locate server",
-			},
-		}
+			}
 	}
 	if server_info.OwnerId != userid {
 		return ServerVerification{
-			Validated: false,
-			Httperr: &HttpErrorInfo{
+				Validated: false,
+			}, &HttpErrorInfo{
 				StatusCode: http.StatusBadRequest,
 				Message:    "error: user not owner of server",
-			},
-		}
+			}
 	}
 
 	return ServerVerification{
 		Validated: true,
 		UserId:    userid,
 		Server:    server_info,
-		Httperr:   nil,
-	}
+	}, nil
 }
 
 func (s *Server) UpdateServer(w http.ResponseWriter, r *http.Request) {
