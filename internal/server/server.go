e90c9b5c057ed36973e2da35ddeea45801cd9f51
m-stanfield <mattstanfield52@gmail.com>
2025-03-23 01:28:59 -0400

    remomoving interfaces to point of consumption

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
@@ -12,6 +13,69 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 
 	"go-chat-react/internal/database"
+)
+
+type UserService interface {
+	GetUserIDFromUserName(username string) (database.Id, error)
+	UpdateUserSessionToken(userid database.Id) (string, time.Time, error)
+	GetUserLoginInfoFromToken(token string) (database.UserLoginInfo, error)
+	GetUserLoginInfo(userid database.Id) (database.UserLoginInfo, error)
+	ValidateUserLoginInfo(userid database.Id, password string) (bool, error)
+
+	GetUser(userid database.Id) (database.User, error)
+	CreateUser(username string, password string) (database.Id, error)
+	UpdateUserName(userid database.Id, username string) error
+	GetRecentUsernames(userid database.Id, number uint) ([]database.UsernameLogEntry, error)
+}
+
+type ServerService interface {
+	GetUsersOfServer(serverid database.Id) ([]database.User, error)
+	GetServersOfUser(userid database.Id) ([]database.Server, error)
+	GetServer(serverid database.Id) (database.Server, error)
+	CreateServer(ownerid database.Id, servername string) (database.Id, error)
+	DeleteServer(serverid database.Id) error
+	UpdateServerName(serverid database.Id, servername string) error
+	IsUserInServer(userid database.Id, serverid database.Id) (bool, error)
+}
+
+type ChannelService interface {
+	AddChannel(serverid database.Id, channelname string) (database.Id, error)
+	DeleteChannel(channelid database.Id) error
+	GetChannel(channelid database.Id) (database.Channel, error)
+	GetChannelsOfServer(serverid database.Id) ([]database.Channel, error)
+	UpdateChannel(channelid database.Id, username string) error
+	AddUserToChannel(channelid database.Id, userid database.Id) error
+	RemoveUserFromChannel(channelid database.Id, userid database.Id) error
+	GetUsersInChannel(channelid database.Id) ([]database.User, error)
+	IsUserInChannel(userid database.Id, channelid database.Id) (bool, error)
+}
+
+type MessageService interface {
+	GetMessage(messageid database.Id) (database.Message, error)
+	GetMessagesInChannel(channelid database.Id, number uint) ([]database.Message, error)
+	AddMessage(channelid database.Id, userid database.Id, message string) (database.Id, error)
+	UpdateMessage(messageid database.Id, message string) error
+	DeleteMessage(messageid database.Id) error
+}
+
+type LifecycleService interface {
+	Close() error
+}
+
+type (
+	AtomicService interface {
+		Service() Service
+		Commit() error
+		Rollback() error
+	}
+
+	Service interface {
+		UserService
+		ServerService
+		ChannelService
+		MessageService
+		LifecycleService
+	}
 )
 
 var (
@@ -51,6 +115,19 @@ func NewInMemoryDB() *database.DBService {
 	return database.New(db)
 }
 
+func Atomic(
+	ctx context.Context,
+	r *database.DBService,
+	opts *sql.TxOptions,
+) (*database.AtomitcDBService, error) {
+	a, err := r.Atomic(ctx, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return a, nil
+}
+
 func NewDB() *database.DBService {
 	// Reuse Connection
 	if dbInstance != nil {
@@ -70,7 +147,7 @@ func NewDB() *database.DBService {
 type Server struct {
 	port int
 
-	db database.Service
+	db Service
 }
 
 func NewServer() *http.Server {
