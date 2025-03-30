893f689c8ae769396dc02c9a5c3d982eabaeec3f
m-stanfield <mattstanfield52@gmail.com>
2025-03-30 13:19:43 -0400

    showing logout only if user is signed in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -77,9 +77,14 @@ function App() {
   }
   return (
     <div className="flex-col h-screen w-screen bg-gray-500 flex py-12 px-4 sm:px-6 lg:px-8 ">
-      <button onClick={auth.logout} className="flex w-full">
+      {auth.authState.isAuthenticated && (
+        <button
+          onClick={auth.logout}
+        className="flex w-32 justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+      >
         Logout
       </button>
+      )}
       {auth.authState.isAuthenticated ? (
         <div className="flex flex-col flex-grow overflow-y-auto">
           <IconBanner
