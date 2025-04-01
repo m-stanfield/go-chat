23b2ae836a1a5f71fcd68ef2e6fdd91eb654d805
m-stanfield <mattstanfield52@gmail.com>
2025-03-31 20:37:24 -0400

    removing other attempt

--- newfrontend/src/components/Home.js
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <div className="flex justify-center items-center mt-10">
-      <h1 className="text-4xl font-bold text-gray-800">Hello World!</h1>
-    </div>
-  );
-}
-
-export default Home; 
