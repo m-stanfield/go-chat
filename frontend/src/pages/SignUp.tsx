6d0c47d109ab93e55d32c8bb061d28433398471a
m-stanfield <mattstanfield52@gmail.com>
2025-04-02 23:14:37 -0400

    adding login and signup

---
+++ frontend/src/pages/SignUp.tsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+
+function SignUp() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmedPassword, setConfirmedPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (password !== confirmedPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    const payload = {
+      username: username,
+      password: password,
+    };
+
+    try {
+      const response = await fetch("/api/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+
+      if (response.ok) {
+        navigate("/login");
+      } else {
+        setError(response.statusText);
+        console.error("Signup failed:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error submitting signup:", error);
+    }
+    console.log("log");
+  };
+
+  return (
+    <div className="flex min-h-full flex-col justify-center">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+          Create a new account
+        </h2>
+      </div>
+
+      <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          {error && (
+            <div className="relative rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700">
+              {error}
+            </div>
+          )}
+
+          <div>
+            <label htmlFor="username" className="block text-sm font-medium leading-6 text-white">
+              Username
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
+              Password
+            </label>
+            <div className="mt-2">
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="confirmpassword"
+              className="block text-sm font-medium leading-6 text-white"
+            >
+              Confirm Password
+            </label>
+            <div className="mt-2">
+              <input
+                type="password"
+                id="confirmpassword"
+                value={confirmedPassword}
+                onChange={(e) => setConfirmedPassword(e.target.value)}
+                required
+                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign up
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div className="mt-6 text-center">
+        <NavLink to="/login">Already have an account? Log in</NavLink>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
