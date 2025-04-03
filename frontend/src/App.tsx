200fa7d9d87458eb90edb2f1d518b830004a666b
m-stanfield <mattstanfield52@gmail.com>
2025-04-02 23:15:45 -0400

    adding login and signup routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { AboutPage } from "./pages/AboutPage";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <div className="flex h-screen w-screen bg-background">
       <main className="flex w-full flex-1 flex-col">
-          <div className="mx-auto w-full px-2 sm:px-4">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/home" element={<HomePage />} />
-              <Route path="/about" element={<AboutPage />} />
-              {/* Add more routes as needed */}
-            </Routes>
-          </div>
+        <div className="mx-auto w-full px-2 sm:px-4">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </div>
       </main>
     </div>
   );
