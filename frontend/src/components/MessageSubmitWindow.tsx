fae9409ab90f600f933b08d65d0af07d34b093bd
m-stanfield <mattstanfield52@gmail.com>
2025-04-12 13:54:54 -0400

    moving onKeydown to out of html code

--- frontend/src/components/MessageSubmitWindow.tsx
+++ frontend/src/components/MessageSubmitWindow.tsx
@@ -3,10 +3,20 @@ import { SyntheticEvent, useState } from "react";
 interface MessageSubmitWindowProps {
     onSubmit: (t: SyntheticEvent, inputValue: string) => string;
 }
-function MessageSubmitWindow(props: MessageSubmitWindowProps) {
+function MessageSubmitWindow({ onSubmit }: MessageSubmitWindowProps) {
     const [inputValue, setInputValue] = useState("");
     const onInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setInputValue(e.target.value);
+    };
+    const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (event.key === "Enter" && event.shiftKey) {
+            event.preventDefault();
+            setInputValue(inputValue + "\n");
+        } else if (event.key === "Enter") {
+            event.preventDefault();
+            const newValue = onSubmit(event, inputValue);
+            setInputValue(newValue);
+        }
     };
     return (
         <div className="flex h-full w-full">
@@ -14,16 +24,7 @@ function MessageSubmitWindow(props: MessageSubmitWindowProps) {
                 name="text"
                 value={inputValue}
                 onChange={onInputChange}
-                onKeyDown={(event) => {
-                    if (event.key === "Enter" && event.shiftKey) {
-                        event.preventDefault();
-                        setInputValue(inputValue + "\n");
-                    } else if (event.key === "Enter") {
-                        event.preventDefault();
-                        const newValue = props.onSubmit(event, inputValue);
-                        setInputValue(newValue);
-                    }
-                }}
+                onKeyDown={onKeyDown}
                 className="flex h-full w-full overflow-auto rounded-lg px-2 text-black"
             ></textarea>
         </div>
