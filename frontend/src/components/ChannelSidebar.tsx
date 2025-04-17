15ace7c621f4bac71d8be587990a3ba59fd48568
m-stanfield <mattstanfield52@gmail.com>
2025-04-16 21:30:21 -0400

    adding create channel button

--- frontend/src/components/ChannelSidebar.tsx
+++ frontend/src/components/ChannelSidebar.tsx
@@ -1,4 +1,5 @@
 import { Channel } from "@/types/channel";
+import { CreateChannelDialog } from "./CreateChannelDialog";
 
 interface ChannelSidebarProps {
     channels: Channel[];
@@ -14,20 +15,26 @@ export default function ChannelSidebar({
     onChannelSelect,
 }: ChannelSidebarProps) {
     return (
-        <div className="w-48 bg-gray-800 p-4 text-white">
+        <div className="flex flex-grow flex-col w-48 bg-gray-800 p-4 text-white">
             <h2 className="mb-4 text-xl font-bold">Channels</h2>
-            <ul className="space-y-2">
-                {channels.map((channel) => (
-                    <li
-                        key={channel.ChannelId}
-                        className={`cursor-pointer rounded p-2 hover:bg-gray-700 ${channel.ChannelId === selectedChannelId ? "bg-gray-700" : ""
-                            }`}
-                        onClick={() => onChannelSelect(channel.ChannelId)}
-                    >
-                        # {channel.ChannelName}
-                    </li>
-                ))}
-            </ul>
+            <div className="flex flex-grow items-start mb-4 overflow-auto">
+                <ul className="space-y-2">
+                    {channels.map((channel) => (
+                        <li
+                            key={channel.ChannelId}
+                            className={`cursor-pointer rounded p-2 hover:bg-gray-700 ${channel.ChannelId === selectedChannelId ? "bg-gray-700" : ""
+                                }`}
+                            onClick={() => onChannelSelect(channel.ChannelId)}
+                        >
+                            # {channel.ChannelName}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="flex flex-col align-bottom mt-4">
+                <CreateChannelDialog serverid={serverid} />
+            </div>
+
         </div>
     );
 }
