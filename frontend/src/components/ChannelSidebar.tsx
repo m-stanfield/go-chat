94f8f8cc0587a892d820f9a4dfd3d13d380b1b86
m-stanfield <mattstanfield52@gmail.com>
2025-04-29 20:33:00 -0400

    scroll only in y

--- frontend/src/components/ChannelSidebar.tsx
+++ frontend/src/components/ChannelSidebar.tsx
@@ -18,7 +18,7 @@ export default function ChannelSidebar({
     return (
         <div className="flex flex-grow flex-col w-48 bg-gray-800 p-4 text-white">
             <h2 className="mb-4 text-xl font-bold">Channels</h2>
-            <div className="flex flex-grow items-start mb-4 overflow-auto">
+            <div className="flex flex-grow items-start mb-4 overflow-y-auto overflow-x-hidden">
                 <ul className="space-y-2">
                     {channels.map((channel) => (
                         <li
