14c4eb9017f2d7c6424513995f0ab009fd4ec3ca
m-stanfield <mattstanfield52@gmail.com>
2025-04-29 23:18:04 -0400

    adding channel create to contxt menu

--- frontend/src/components/SidebarContextMenu.tsx
+++ frontend/src/components/SidebarContextMenu.tsx
@@ -1,27 +1,54 @@
 
 
+import { useState } from "react"
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogDescription,
+    DialogFooter,
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button"
 import {
     ContextMenu,
     ContextMenuContent,
     ContextMenuItem,
     ContextMenuTrigger,
 } from "@/components/ui/context-menu"
+import { DialogTrigger } from "@radix-ui/react-dialog";
+import { CreateChannelDialog } from "./CreateChannelDialog";
 
 interface SidebarContextMenuProps {
     children: React.ReactNode;
+    serverid: number;
     className?: string;
 };
-export default function SidebarContextMenu({ children, className }: SidebarContextMenuProps) {
+export default function SidebarContextMenu({ serverid, children, className }: SidebarContextMenuProps) {
+
+    const [dialogOpen, setDialogOpen] = useState(false)
     return (
-        <ContextMenu>
-            <ContextMenuTrigger asChild>
-                <div className={className}>
-                    {children}
-                </div >
-            </ContextMenuTrigger>
-            <ContextMenuContent>
-                <ContextMenuItem>Channel Sidebar</ContextMenuItem>
-            </ContextMenuContent>
-        </ContextMenu>
+        <>
+
+            <Dialog >
+                <ContextMenu>
+                    <ContextMenuTrigger asChild>
+                        <div className={className}>
+                            {children}
+                        </div >
+                    </ContextMenuTrigger>
+                    <ContextMenuContent>
+                        <DialogTrigger asChild>
+                            <ContextMenuItem onClick={() => setDialogOpen(true)}>
+                                Create Channel
+                            </ContextMenuItem>
+                        </DialogTrigger>
+                    </ContextMenuContent>
+                </ContextMenu>
+
+                <CreateChannelDialog open={dialogOpen} setOpen={setDialogOpen} serverid={serverid} />
+
+            </Dialog>
+        </>
     );
 }
