bc94a16ac0d44b562d7cca3ccaae438a32936e2d
m-stanfield <mattstanfield52@gmail.com>
2025-05-01 21:35:46 -0400

    fixing scrolling

--- frontend/src/pages/ServerPage.tsx
+++ frontend/src/pages/ServerPage.tsx
@@ -164,7 +164,7 @@ function ServerPage({ server_id, number_of_messages }: ServerPageProps) {
 
   return (
     <div className="flex flex-grow">
-      <SidebarContextMenu serverid={server_id} className="flex flex-shrink-0">
+      <SidebarContextMenu serverid={server_id} className="flex w-full h-full">
         <ChannelSidebar
           channels={channels}
           serverid={server_id}
