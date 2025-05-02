675044810d898f88f499d98d3f67502da35a0bdd
m-stanfield <mattstanfield52@gmail.com>
2025-05-01 20:47:31 -0400

    commiting

--- frontend/src/components/ChannelSidebar.tsx
+++ frontend/src/components/ChannelSidebar.tsx
@@ -1,6 +1,4 @@
 import { Channel } from "@/types/channel";
-import { CreateChannelDialog } from "./CreateChannelDialog";
-import { useState } from "react";
 import ChannelSidebarContextMenu from "./ChannelContextMenu";
 
 interface ChannelSidebarProps {
@@ -16,11 +14,10 @@ export default function ChannelSidebar({
     selectedChannelId,
     onChannelSelect,
 }: ChannelSidebarProps) {
+    console.log("ChannelSidebar", serverid);
 
-    const [open, setOpen] = useState(false);
     return (
         <>
-            <CreateChannelDialog serverid={serverid} open={open} setOpen={setOpen} />
             <div className="flex flex-grow flex-col w-48 bg-gray-800 p-4 text-white">
                 <h2 className="mb-4 text-xl font-bold">Channels</h2>
                 <div className="flex flex-grow items-start mb-4 overflow-y-auto overflow-x-hidden">
